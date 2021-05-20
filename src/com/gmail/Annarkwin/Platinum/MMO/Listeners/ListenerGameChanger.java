70c4ba2bb092b0981986bcffecaffc1f6c6ecb05
Annarkwin <annarkwin@gmail.com>
2021-05-20 16:35:33 -0500

    Armorstands are small if placed on redstone, endermen should no longer mess with blocks

--- src/com/gmail/Annarkwin/Platinum/MMO/Listeners/ListenerGameChanger.java
+++ src/com/gmail/Annarkwin/Platinum/MMO/Listeners/ListenerGameChanger.java
@@ -1,15 +1,22 @@
 package com.gmail.Annarkwin.Platinum.MMO.Listeners;
 
 import org.bukkit.Material;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.Enderman;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBurnEvent;
 import org.bukkit.event.block.BlockSpreadEvent;
+import org.bukkit.event.entity.EntityChangeBlockEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+
+
 import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.event.entity.EntityInteractEvent;
+import org.bukkit.event.entity.EntitySpawnEvent;
 
 public class ListenerGameChanger implements Listener
 {
@@ -68,5 +75,55 @@ public class ListenerGameChanger implements Listener
 			e.blockList().clear();
 
 	}
+
+	// Change spawned armorstands to have arms
+	@EventHandler(ignoreCancelled = true, priority = EventPriority.HIGH)
+	public void armorStandAddArms( EntitySpawnEvent e )
+	{
+
+		if (e.getEntity() instanceof ArmorStand)
+		{
+
+			ArmorStand a = (ArmorStand) e.getEntity();
+			a.setArms(true);
+			a.setCanPickupItems(true);
+			if (a.getLocation().getBlock().getType() == Material.REDSTONE_WIRE)
+				a.setSmall(true);
+
+		}
+
+	}
+
+	// Endermen no longer ruin terrain
+	@EventHandler(ignoreCancelled = true, priority = EventPriority.HIGH)
+	public void endermanTouchStuff( EntityChangeBlockEvent e )
+	{
+
+		if (e.getEntity() instanceof Enderman)
+		{
+
+			e.setCancelled(true);
+
+		}
+
+	}
+
+	// powered armorstands have a pose
+
+//	// Change spawned armorstands to have arms
+//	@EventHandler(ignoreCancelled = true, priority = EventPriority.HIGH)
+//	public void armorStandPowerPose( BlockRedstoneEvent e )
+//	{
+//
+//		Collection<Entity> mobs = e.getBlock().getWorld().getNearbyEntities(e.getBlock().getBoundingBox());
+//		
+//		for (Entity target : mobs) {
+//			if (target.getType() == EntityType.ARMOR_STAND)
+//			{
+//				
+//			}
+//		}
+//
+//	}
 
 }
